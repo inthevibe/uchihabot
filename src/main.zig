34921687f9be8342d5e7ecea3679121d19576038
InTheVibe <andersonsribeiro@proton.me>
2025-03-15 23:06:24 -0300

    Trying out how to add a dependency, and adding a http server dependency

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,25 @@
 const std = @import("std");
+const httpz = @import("httpz");
 
 pub fn main() !void {
-    std.debug.print("Hello World!!", .{});
+    std.debug.print("Hello World!!\n", .{});
+
+    var gpa = std.heap.DebugAllocator(.{}){};
+    const allocator = gpa.allocator();
+
+    var server = try httpz.Server(void).init(allocator, .{ .port = 5882 }, {});
+    defer {
+        server.stop();
+        server.deinit();
+    }
+
+    var router = try server.router(.{});
+    router.get("hello", helloWorld, .{});
+
+    try server.listen();
+}
+
+fn helloWorld(_: *httpz.Request, res: *httpz.Response) !void {
+    res.status = 200;
+    try res.json(.{ .message = "Hello, World!" }, .{});
 }
